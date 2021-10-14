d85d25502a4d9bf88c7f8995b68ae5eb6efc6baf
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-10-15 03:45:04 +0600

    updated

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,9 @@ import subprocess
 inputFileCSV: str = "files/test.csv"
 outputFileCSV: str = "test.csv"
 
+inputFileTSV: str = "files/test.tsv"
+outputFileTSV: str = "test.tsv"
+
 inputFileXLS: str = "files/test.xls"
 outputFileXLS: str = "test.xls"
 
@@ -34,6 +37,18 @@ def test_csv_to_csv():
     output = output.decode("utf-8").strip()
     os.remove(outputFileCSV)
     assert output == "The file is already in csv format."
+
+
+# CSV to TSV ↓
+def test_csv_to_tsv():
+    global inputFileCSV, outputFileTSV
+    shutil.copyfile(inputFileCSV, outputFileTSV)
+    output = subprocess.check_output(
+        ["python", "-m", "rubaialter", inputFileCSV, "--tsv"]
+    )
+    output = output.decode("utf-8").strip()
+    os.remove(outputFileTSV)
+    assert output == "The file is already in tsv format."
 
 
 # CSV to XLS ↓
@@ -78,6 +93,14 @@ def test_xls_to_csv():
     os.remove("Sheet1.csv")
 
 
+# XLS to TSV
+def test_xls_to_tsv():
+    global inputFileXLS, outputFileTSV
+    subprocess.check_call(["python", "-m", "rubaialter", inputFileXLS, "--tsv"])
+    assert os.path.exists("Sheet1.tsv")
+    os.remove("Sheet1.tsv")
+
+
 # xls to XLSX ↓
 def test_xls_to_xlsx():
     global inputFileXLS, outputFileXLSX
@@ -111,6 +134,14 @@ def test_xlsx_to_csv():
     subprocess.check_call(["python", "-m", "rubaialter", inputFileXLSX, "--csv"])
     assert os.path.exists("Sheet1.csv")
     os.remove("Sheet1.csv")
+
+
+# XLSX to CSV
+def test_xlsx_to_tsv():
+    global inputFileXLSX, outputFileTSV
+    subprocess.check_call(["python", "-m", "rubaialter", inputFileXLSX, "--tsv"])
+    assert os.path.exists("Sheet1.tsv")
+    os.remove("Sheet1.tsv")
 
 
 # XLSX to XLS ↓
@@ -149,6 +180,14 @@ def test_sqlite3_to_csv():
     os.remove("Sheet1.csv")
 
 
+# SQLITE3 to CSV
+def test_sqlite3_to_csv():
+    global inputFileSQLITE3, outputFileTSV
+    subprocess.check_call(["python", "-m", "rubaialter", inputFileSQLITE3, "--tsv"])
+    assert os.path.exists("Sheet1.tsv")
+    os.remove("Sheet1.tsv")
+
+
 # SQLITE3 to XLS ↓
 def test_sqlite3_to_xls():
     global inputFileSQLITE3, outputFileXLS
@@ -169,14 +208,25 @@ def test_sqlite3_to_xlsx():
 def test_csv_to_xls_xlsx_sqlite3():
     global inputFileCSV, outputFileXLS
     subprocess.check_call(
-        ["python", "-m", "rubaialter", inputFileCSV, "--xls", "--xlsx", "--sqlite3"]
+        [
+            "python",
+            "-m",
+            "rubaialter",
+            inputFileCSV,
+            "--xls",
+            "--xlsx",
+            "--sqlite3",
+            "--tsv",
+        ]
     )
     assert os.path.exists(outputFileXLS)
     assert os.path.exists(outputFileXLSX)
     assert os.path.exists(outputFileSQLITE3)
+    assert os.path.exists(outputFileTSV)
     os.remove(outputFileXLS)
     os.remove(outputFileXLSX)
     os.remove(outputFileSQLITE3)
+    os.remove(outputFileTSV)
 
 
 # Cleanup ↓
